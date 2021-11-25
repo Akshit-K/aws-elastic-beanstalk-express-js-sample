b921140cdb5a6d20af366567d72ef359035eaf69
Akshit <st122042@ait.asia>
2021-11-25 18:25:33 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('delivery pipeline test'));
+app.get('/', (req, res) => res.send('Delivery pipeline project using AWS by setting up Git repository'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
